5101c7fdac33be9d5036b709837b9b9b79568f01
mac <michaelemadso4@gmail.com>
2024-09-25 12:46:24 +0300

    toastmessag.dart added

---
+++ lib/helper/ToastMessag/toastmessag.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+void showToast({required String text,required ToastState state})=> Fluttertoast.showToast(
+    msg: "$text",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 3,
+    backgroundColor: choasToastColor(state),
+    textColor: Colors.white,
+    fontSize: 16.0
+);
+enum ToastState{
+  SUCCESS,ERROR,WARNING
+}
+Color choasToastColor(ToastState state){
+  Color color;
+  switch(state){
+    case ToastState.SUCCESS:
+      color= Colors.green;
+      break;
+    case ToastState.ERROR:
+      color= Colors.red.shade900;
+      break;
+    case ToastState.WARNING:
+      color = Colors.amber;
+      break;
+  }
+  return color;
+}
